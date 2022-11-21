16868c9403b49a56c512691ecaba463ceb91945f
jezisgamer <78708901+jezisgamer@users.noreply.github.com>
2022-11-20 18:33:27 -0600

    Create caxpc v1 for apk based linux.sh

---
+++ caxpc v1 for apk based linux.sh
@@ -0,0 +1,16 @@
+apk update
+apk add sudo
+sudo apk update
+sudo apk add xfce gnome kwrite gdm3
+sh <(curl -L https://nixos.org/nix/install) --daemon
+sudo dpkg-reconfigure gdm3
+sudo apk update 
+sudo apk add thunar
+sudo apk update
+sudo apk add nordic 
+sudo apk update
+sudo apk add firefox
+mkdir .themes 
+git clone https://github.com/EliverLara/Sweet.git 
+cd
+reboot
